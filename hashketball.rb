aab77a893e63f5e1c19185b373070b054441f728
congenial-course-2234 <jakelozano94@gmail.com>
2020-07-10 01:20:59 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -209,7 +209,7 @@ game_hash.each do |team, team_info|
   team_info[:players].each do |attributes|
     if attributes[:shoe] == size
       answer = attributes[:rebounds]
-      binding.pry
+     # binding.pry
     end
   end
 end
